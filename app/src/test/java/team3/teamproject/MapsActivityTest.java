22a5cd85da7b9cab75d36ec90c21e15f628ae5aa
Stephen Northrop <Stephen.Northrop@hotmail.com>
2018-04-23 13:12:22 +0100

    added a few tests

--- app/src/test/java/team3/teamproject/MapsActivityTest.java
+++ app/src/test/java/team3/teamproject/MapsActivityTest.java
@@ -41,6 +41,13 @@ public class MapsActivityTest {
 
     /*getSensorFromType*/
     @Test
+    public void getSensorFromType_validTypeTemperature_returnsList(){
+        MapsActivity maps = new MapsActivity();
+        OverlayState temperature = OverlayState.Temperature;
+        assertNotEquals(maps.getSensorsFromType(temperature),0);
+    }
+
+    @Test
     public void getSensorFromType_validTypeSound_returnsList(){
         MapsActivity maps = new MapsActivity();
         OverlayState sound = OverlayState.Sound;
@@ -48,46 +55,87 @@ public class MapsActivityTest {
     }
 
     @Test
-    public void getSensorFromType_validTypeAir_returnsList(){
+    public void getSensorFromType_validTypeHumidity_returnsList(){
         MapsActivity maps = new MapsActivity();
-        OverlayState sound = OverlayState.Air;
-        assertNotEquals(maps.getSensorsFromType(sound),0);
+        OverlayState humidity = OverlayState.Humidity;
+        assertNotEquals(maps.getSensorsFromType(humidity),0);
+    }
+    @Test
+    public void getSensorFromType_validTypeNO2_returnsList(){
+        MapsActivity maps = new MapsActivity();
+        OverlayState NO2 = OverlayState.NO2;
+        assertNotEquals(maps.getSensorsFromType(NO2),0);
+    }
+    @Test
+    public void getSensorFromType_validTypeNO_returnsList(){
+        MapsActivity maps = new MapsActivity();
+        OverlayState NO = OverlayState.NO;
+        assertNotEquals(maps.getSensorsFromType(NO),0);
     }
 
     @Test
     public void getSensorFromType_validTypeCO_returnsList(){
         MapsActivity maps = new MapsActivity();
-        OverlayState sound = OverlayState.CO;
-        assertNotEquals(maps.getSensorsFromType(sound),0);
+        OverlayState CO = OverlayState.CO;
+        assertNotEquals(maps.getSensorsFromType(CO),0);
     }
 
     @Test
-    public void getSensorFromType_validTypeHumidity_returnsList(){
+    public void getSensorFromType_validTypeWindSpeed_returnsList(){
         MapsActivity maps = new MapsActivity();
-        OverlayState sound = OverlayState.Humidity;
-        assertNotEquals(maps.getSensorsFromType(sound),0);
+        OverlayState wind = OverlayState.Wind_speed;
+        assertNotEquals(maps.getSensorsFromType(wind),0);
+    }
+    @Test
+    public void getSensorFromType_validTypeNOX_returnsList(){
+        MapsActivity maps = new MapsActivity();
+        OverlayState NOX = OverlayState.NOX;
+        assertNotEquals(maps.getSensorsFromType(NOX),0);
+    }
+    @Test
+    public void getSensorFromType_validTypeO3_returnsList(){
+        MapsActivity maps = new MapsActivity();
+        OverlayState O3 = OverlayState.O3;
+        assertNotEquals(maps.getSensorsFromType(O3),0);
+    }
+    @Test
+    public void getSensorFromType_validTypePressure_returnsList(){
+        MapsActivity maps = new MapsActivity();
+        OverlayState Pressure = OverlayState.Pressure;
+        assertNotEquals(maps.getSensorsFromType(Pressure),0);
+    }
+    @Test
+    public void getSensorFromType_validTypeRainAccumulation_returnsList(){
+        MapsActivity maps = new MapsActivity();
+        OverlayState Rain_Accumulation = OverlayState.Rain_Accumulation;
+        assertNotEquals(maps.getSensorsFromType(Rain_Accumulation),0);
+    }
+    @Test
+    public void getSensorFromType_validTypeRainFall_returnsList(){
+        MapsActivity maps = new MapsActivity();
+        OverlayState Rain_Fall = OverlayState.Rain_Fall;
+        assertNotEquals(maps.getSensorsFromType(Rain_Fall),0);
+    }
+    @Test
+    public void getSensorFromType_validTypeSewageLevel_returnsList(){
+        MapsActivity maps = new MapsActivity();
+        OverlayState SewageLevel = OverlayState.Sewage_Level;
+        assertNotEquals(maps.getSensorsFromType(SewageLevel),0);
+    }
+    @Test
+    public void getSensorFromType_validTypeSolarDiffuseRadiation_returnsList(){
+        MapsActivity maps = new MapsActivity();
+        OverlayState Solar = OverlayState.Solar_Diffuseradiation;
+        assertNotEquals(maps.getSensorsFromType(Solar),0);
+    }
+    @Test
+    public void getSensorFromType_validTypeVisiblity_returnsList(){
+        MapsActivity maps = new MapsActivity();
+        OverlayState Visiblity = OverlayState.Visiblity;
+        assertNotEquals(maps.getSensorsFromType(Visiblity),0);
     }
 
-    @Test
-    public void getSensorFromType_validTypeNO_returnsList(){
-        MapsActivity maps = new MapsActivity();
-        OverlayState sound = OverlayState.NO;
-        assertNotEquals(maps.getSensorsFromType(sound),0);
-    }
 
-    @Test
-    public void getSensorFromType_validTypeNO2_returnsList(){
-        MapsActivity maps = new MapsActivity();
-        OverlayState sound = OverlayState.NO2;
-        assertNotEquals(maps.getSensorsFromType(sound),0);
-    }
-
-    @Test
-    public void getSensorFromType_validTypeTemperature_returnsList(){
-        MapsActivity maps = new MapsActivity();
-        OverlayState sound = OverlayState.Temperature;
-        assertNotEquals(maps.getSensorsFromType(sound),0);
-    }
 
     @Test
     public void getAllSensorData_returnList(){
